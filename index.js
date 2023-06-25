a52972c5bf360141421d88fd9aa2221286e87dd7
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-25 21:59:42 +0530

    new

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const express = require('express')
+const express = require('express');
 const { connectToMongoDB } = require('./connect');
-const urlRoute = require('./routes/url')
-const URL = require('./models/url')
+const cors = require('cors');
+const urlRoute = require('./routes/url');
+const URL = require('./models/url');
 
-const app = express()
+const app = express();
 
-connectToMongoDB('mongodb+srv://siddiq:siddiq12345@cluster0.4ghwomw.mongodb.net/?retryWrites=true&w=majority').then(()=> console.log('Mongodb connected'))
+app.use(cors()); // Enable CORS
 
-app.use(express.json())
-app.use("/url", urlRoute)
+connectToMongoDB('mongodb+srv://siddiq:siddiq12345@cluster0.4ghwomw.mongodb.net/?retryWrites=true&w=majority').then(() =>
+  console.log('Mongodb connected')
+);
 
-app.get('/:shortId', async(req, res) => {
-    const shortId = req.params.shortId; 
-    const entry = await URL.findOneAndUpdate({
-        shortId
+app.use(express.json());
+app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
     },
-     { 
-        $push:{
-        visitHistory:{
-            timestamp: Date.now(),
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
         },
-    },
- })  
- res.redirect(entry.redirectURL)
-})
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
 
-app.listen(5000, () => console.log("server "))
+app.listen(5000, () => console.log('server is running on port 5000'));
