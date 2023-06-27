4bae3891b52312ff616ba696ce74845012cbb996
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-27 16:06:48 +0530

    new

--- routes/url.js
+++ routes/url.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { hanleGenerateNewShortURL, handleGetAnalytics, handleGetAllURLs, handleGetDailyCount, handleGetMonthlyCount } = require('../controllers/url')
 const router = express.Router();
  
-router.get('/dailyCount', handleGetDailyCount);
-router.get('/monthlyCount', handleGetMonthlyCount);
+router.post('/dailyCount', handleGetDailyCount);
+router.post('/monthlyCount', handleGetMonthlyCount);
 router.get('/all', handleGetAllURLs);
 router.get('/analytics/:shortId', handleGetAnalytics);
 router.post('/', hanleGenerateNewShortURL);
